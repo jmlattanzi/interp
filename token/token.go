d52c7f4d4b515bab1c148bddc054a034ace0221e
jmlattanzi <jonathanlattanzi@gmail.com>
2019-12-27 09:29:47 -0600

    constants weren't committed?

--- token/token.go
+++ token/token.go
@@ -1,9 +1,14 @@
 package token
 
+// TokenType is basically the enum value
 type TokenType string
+
+// Token is the definition of what a token is
+// you can optionally store metadata about the token
+// like the line and column number
 type Token struct {
-	Type    TokenType
-	Literal string
+	Type    TokenType // the type, kinda like the enum
+	Literal string    // the actual character itself
 }
 
 const (
@@ -15,8 +20,17 @@ const (
 	INT   = "INT"
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	BANG     = "!"
+	ASTERISK = "*"
+	MINUS    = "-"
+	SLASH    = "/"
+
+	GT = ">"
+	LT = "<"
+	EQ = "=="
+	NOT_EQ = "!="
 
 	// delimiters
 	COMMA     = ","
@@ -30,4 +44,27 @@ const (
 	// keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
+}
+
+// LookupIdentifier finds the keyword used in our keyword map
+func LookupIdentifier(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
